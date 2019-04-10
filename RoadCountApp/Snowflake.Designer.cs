36aacef356f66699b1a0f73d50923044b219ac50
AblyazovDavlat <ablyazovdavlat@gmail.com>
2019-04-10 20:46:30 +0300

    Implemented app

---
+++ RoadCountApp/Snowflake.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace RoadCountApp
+{
+    partial class SnowflakeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.inputLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.inputTextBox.Location = new System.Drawing.Point(12, 36);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(285, 20);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // inputLabel
+            // 
+            this.inputLabel.AutoSize = true;
+            this.inputLabel.Location = new System.Drawing.Point(13, 17);
+            this.inputLabel.Name = "inputLabel";
+            this.inputLabel.Size = new System.Drawing.Size(31, 13);
+            this.inputLabel.TabIndex = 1;
+            this.inputLabel.Text = "Input";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(98, 148);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(108, 45);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "Go!";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // SnowflakeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 205);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.inputLabel);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "SnowflakeForm";
+            this.Text = "Snowflake planet";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Label inputLabel;
+        private System.Windows.Forms.Button startButton;
+    }
+}
+
---
+++ RoadCountApp/Snowflake.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RoadCountApp
+{
+    public partial class SnowflakeForm : Form
+    {
+        public SnowflakeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            string filePath;
+            string[] readFile;
+            ByteMatrix byteMatrix;
+            int resultCount;
+
+            if (inputTextBox.Text.Length != 0)
+            {
+                filePath = inputTextBox.Text;
+            }
+            else
+            {
+                Console.WriteLine("Please. enter path ro input file");
+                return;
+            }
+
+            if (File.Exists(filePath))
+            {
+                readFile = File.ReadAllLines(filePath);
+            }
+            else
+            {
+                Console.WriteLine("File is not exist");
+                return;
+            }
+
+            byteMatrix = new ByteMatrix(Int32.Parse(readFile[0]));
+
+            //Parse array of strings into matrix
+            for (int i = 1; i < readFile.Length; i++)
+            {
+                byteMatrix.matrix[i-1] = readFile[i].Split(' ').ToArray().Select(j => Byte.Parse(j.ToString())).ToArray();
+            }
+
+            resultCount = CountNumberRoads(byteMatrix);
+            File.WriteAllText(Path.GetDirectoryName(filePath) + "\\output.txt" ,resultCount.ToString());
+            startButton.Text = "Done";
+        }
+
+        //ByteMatrix byteMatrix - NxN, nodes values: 0 or 1
+        //int count - number roads in matrix
+        //Input matrix is symmetrical, because count number of ones in the right triangle of the matrix.
+        private int CountNumberRoads(ByteMatrix byteMatrix)
+        {
+            int count = 0;
+
+            for (int i = 0; i < byteMatrix.N; i++)
+            {
+                for (int j = i; j < byteMatrix.N; j++)
+                {
+                    count += byteMatrix.matrix[i][j];
+                }
+            }
+            return count;
+        }
+    }
+
+    public class ByteMatrix
+    {
+        public byte[][] matrix;
+        public int N;
+
+        public ByteMatrix()
+        {
+            matrix = new byte[0][];
+            this.N = 0;
+        }
+
+        public ByteMatrix(int N)
+        {
+            matrix = new byte[N][];
+            this.N = N;
+        }
+    }
+}
